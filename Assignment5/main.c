db1f2df9eecd220f800f47506a1b5c405b3b53b1
Nicolas Emery <Nicolas@Nicolass-MacBook-Pro.local>
2014-12-08 19:06:12 -0500

    Initial Commit

---
+++ Assignment5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment5
+//
+//  Created by Nicolas Emery on 12/8/14.
+//  Copyright (c) 2014 Nicolas Emery. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
